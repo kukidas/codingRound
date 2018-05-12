14b1071fc87b513002f0c0f9ff82af1fe875bb99
Kuki Das <kukidas09@gmail.com>
2018-05-12 15:55:37 +0530

    Changes included in this commit - HotelBookingTest:
    1. Initializing page objects via pagefactory
    2. Selecting the first option from the auto suggest drop down for Location field
    3. Selecting the check in date from DatePicker
    4. Selecting the check out date from DatePicker
    5. added appropriate wait @different places

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,10 +1,16 @@
 import com.sun.javafx.PlatformUtil;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
@@ -22,29 +28,53 @@ public class HotelBookingTest {
 
     @FindBy(id = "travellersOnhome")
     private WebElement travellerSelection;
-    
-    @BeforeTest
+       
+    @BeforeClass
     public void initialize(){
     	setDriverPath();
-    	driver  = new ChromeDriver();    	
+    	driver  = new ChromeDriver();   
+    	//initializing page objects via pagefactory
+    	PageFactory.initElements(driver, this); 
     }
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
         
     	driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+    	hotelLink.click();
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
-
+        
+        waitFor(1000);
+        
+        //Selecting the first option from the auto suggest drop down
+        List<WebElement> list= driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        list.get(1).click();
+        
+        //Selecting the check in date from DatePicker
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[1]/a")).click();
+        waitFor(2000);
+        
+       //Selecting the check out date from DatePicker
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[2]/table/tbody/tr[4]/td[3]/a")).click();
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+        
+        waitFor(2000);
         searchButton.click();
 
         driver.quit();
 
     }
-
-    private void setDriverPath() {
+    
+    private void waitFor(int durationInMilliSeconds) {
+        try {
+            Thread.sleep(durationInMilliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+    
+   private void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_mac");
         }
